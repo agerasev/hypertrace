b6ec07243e74caaf3f5ab87c661956eff54575dc
Alexey Gerasev <alexey.gerasev@gmail.com>
2019-01-30 10:52:13 +0700

    opencl test

---
+++ src/main.rs
@@ -0,0 +1,34 @@
+extern crate ocl;
+use ocl::ProQue;
+
+fn trivial() -> ocl::Result<()> {
+    let src = r#"
+        __kernel void add(__global float* buffer, float scalar) {
+            buffer[get_global_id(0)] += scalar;
+        }
+    "#;
+
+    let pro_que = ProQue::builder()
+        .src(src)
+        .dims(1 << 20)
+        .build()?;
+
+    let buffer = pro_que.create_buffer::<f32>()?;
+
+    let kernel = pro_que.kernel_builder("add")
+        .arg(&buffer)
+        .arg(10.0f32)
+        .build()?;
+
+    unsafe { kernel.enq()?; }
+
+    let mut vec = vec![0.0f32; buffer.len()];
+    buffer.read(&mut vec).enq()?;
+
+    println!("The value at index [{}] is now '{}'!", 200007, vec[200007]);
+    Ok(())
+}
+
+fn main() {
+	trivial().unwrap();
+}

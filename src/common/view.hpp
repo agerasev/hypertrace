9919e74dc3acfe0dd591a8397f378926c5aeee9a
Alexey Gerasev <alexey.gerasev@gmail.com>
2020-04-13 14:49:22 +0700

    Converting view to device format

--- src/common/view.hpp
+++ src/common/view.hpp
@@ -1,7 +1,6 @@
 #pragma once
 
 #include <types.h>
-
 #include <algebra/moebius.hpp>
 
 
@@ -12,3 +11,30 @@ public:
     real lens_radius = 1e-2_r;
     real focal_length = 5.0_r;
 };
+
+#ifdef HOST
+namespace device {
+struct View {
+    MoebiusT<::complex<device::real>> pos;
+    device::real fov;
+    device::real lr;
+    device::real fl;
+};
+}
+
+template <>
+struct ToDevice<View> {
+    typedef device::View type;
+    static type to_device(View v) {
+        return type {
+            .pos = MoebiusT<complex<device::real>>(
+                convert<matrix<device::real, 2, 2>>(v.position.mat())
+            ),
+            .fov = v.field_of_view,
+            .lr = v.lens_radius,
+            .fl = v.focal_length
+        };
+    }
+};
+
+#endif

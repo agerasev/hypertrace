d88d5fa5f08f4fddec91a7ffac0cb78e581ce857
Alexey Gerasev <alexey.gerasev@gmail.com>
2021-01-03 22:17:37 +0700

    Fix CI script

--- manage/__init__.py
+++ manage/__init__.py
@@ -3,7 +3,7 @@ from manage.component import *
 
 components = [
     ("rstd", Cmake("rstd", test="rstd_test")),
-    ("core", Cmake("core")),
+    ("core", Cmake("core", test="kernel_test")),
     ("vecmat", Cargo("vecmat")),
     ("dyntype", Cargo("dyntype")),
 ]

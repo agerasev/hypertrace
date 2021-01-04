2014943a969cb359b3d3f03256c91a0dae04eac1
Alexey Gerasev <alexey.gerasev@gmail.com>
2021-01-05 00:15:38 +0700

    WIP: Macros

--- kernel/src/macros.cc
+++ kernel/src/macros.cc
@@ -5,14 +5,41 @@
 #include <gtest/gtest.h>
 #include <string>
 
-#define STRINGIFY(a) #a
-#define STRINGIFY_EX(a) STRINGIFY(a)
+#define _STRINGIFY(a) #a
+#define STRINGIFY(a) _STRINGIFY(a)
 
-#define ABC_ abc_
+TEST(MacrosTest, va_count) {
+    ASSERT_EQ(1, VA_COUNT(A));
+    ASSERT_EQ(2, VA_COUNT(A,B));
+    ASSERT_EQ(3, VA_COUNT(A,B,C));
+    ASSERT_EQ(4, VA_COUNT(A,B,C,D));
+    ASSERT_EQ(5, VA_COUNT(A,B,C,D,E));
+    ASSERT_EQ(6, VA_COUNT(A,B,C,D,E,F));
+    ASSERT_EQ(7, VA_COUNT(A,B,C,D,E,F,G));
+    ASSERT_EQ(8, VA_COUNT(A,B,C,D,E,F,G,H));
+}
+
+#define A a
 
 TEST(MacrosTest, concat) {
-    ASSERT_EQ(std::string("abc_def"), std::string(STRINGIFY_EX(CONCAT(abc_, def))));
-    ASSERT_EQ(std::string("abc_def"), std::string(STRINGIFY_EX(CONCAT_EX(ABC_, def))));
+    ASSERT_EQ(std::string("ab"), std::string(STRINGIFY(_CONCAT_2(a, b))));
+    ASSERT_EQ(std::string("Ab"), std::string(STRINGIFY(_CONCAT_2(A, b))));
+    ASSERT_EQ(std::string("ab"), std::string(STRINGIFY(CONCAT_2(A, b))));
+    ASSERT_EQ(std::string("abc"), std::string(STRINGIFY(CONCAT_3(a, b, c))));
+
+    //ASSERT_EQ(std::string("ab"), std::string(STRINGIFY(CONCAT(a, b))));
+}
+
+#define C2(x, y) x##y
+#define C3(x, y, z) x##y##z
+
+#define GET_MACRO(_1,_2,_3,NAME,...) NAME
+#define C(...) GET_MACRO(__VA_ARGS__, C3, C2)(__VA_ARGS__)
+
+#define X a
+
+TEST(MacrosTest, test) {
+    ASSERT_EQ(std::string("abc"), std::string(STRINGIFY(C(X, b, c))));
 }
 
 #endif // UNITTEST
--- kernel/src/macros.hh
+++ kernel/src/macros.hh
@@ -1,10 +1,27 @@
 #pragma once
 
-// Concatenate `a` and `b`
-#define CONCAT(a, b) a##b
+// Concatenate `A` and `B` without macro expansion.
+#define _CONCAT_2(A, B) A##B
 
-// Expand `a` and `b` and concatenate the results
-#define CONCAT_EX(a, b) CONCAT(a, b)
+// Expand `A` and `B` and concatenate the results.
+#define CONCAT_2(A, B) _CONCAT_2(A, B)
+
+#define CONCAT_1(A) A
+
+#define EVAL(...) __VA_ARGS__
+#define _VA_COUNT(_8,_7,_6,_5,_4,_3,_2,_1,_N,...) _N
+#define VA_COUNT(...) _VA_COUNT(__VA_ARGS__,8,7,6,5,4,3,2,1)
+
+#define CONCAT_3(X, ...) CONCAT_2(X, CONCAT_2(__VA_ARGS__))
+#define CONCAT_4(X, ...) CONCAT_2(X, CONCAT_3(__VA_ARGS__))
+#define CONCAT_5(X, ...) CONCAT_2(X, CONCAT_4(__VA_ARGS__))
+#define CONCAT_6(X, ...) CONCAT_2(X, CONCAT_5(__VA_ARGS__))
+#define CONCAT_7(X, ...) CONCAT_2(X, CONCAT_6(__VA_ARGS__))
+#define CONCAT_8(X, ...) CONCAT_2(X, CONCAT_7(__VA_ARGS__))
+
+#define CONCAT(...) CONCAT_2(CONCAT_, VA_COUNT(__VA_ARGS__))(__VA_ARGS__)
+
+#define $(...) CONCAT(__VA_ARGS__)
 
 #ifndef HOST
 #include "macros.cc"

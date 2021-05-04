24d374b172a53ae841cd6ea0d410fd6f1bee098f
Alexey Gerasev <alexey.gerasev@gmail.com>
2021-05-04 19:45:30 +0700

    Specialize chain

--- types/src/map.rs
+++ types/src/map.rs
@@ -1,6 +1,6 @@
-use crate::{Entity, SizedEntity, Config, source::{Sourced, SourceInfo}, math, io::{CntRead, CntWrite}};
+use crate::{Entity, SizedEntity, Config, source::{Sourced, SourceInfo, SourceBuilder}, math, io::{CntRead, CntWrite}, include_template};
 use vecmat::{Vector, Matrix, complex::{Complex, Quaternion}, transform::{Shift, Linear, Rotation2, Rotation3, Moebius, Chain, Reorder}};
-use std::io;
+use std::{io, any::TypeId};
 use ccgeom::Map as GeoMap;
 
 pub trait Map<P, D = P>: GeoMap<P, D> + SizedEntity + Sourced {}
@@ -17,8 +17,7 @@ impl SizedEntity for Shift<f64, 2> {
 }
 impl Sourced for Shift<f64, 2> {
     fn source(_: &Config) -> SourceInfo {
-        unimplemented!()
-        //SourceInfo::with_root("Shift2", "shf2", "algebra/shift.hh")
+        SourceInfo::with_root("Shift2", "shf2", "algebra/shift.hh")
     }
 }
 
@@ -30,8 +29,7 @@ impl SizedEntity for Shift<f64, 3> {
 }
 impl Sourced for Shift<f64, 3> {
     fn source(_: &Config) -> SourceInfo {
-        unimplemented!()
-        //SourceInfo::with_root("Shift3", "shf3", "algebra/shift.hh")
+        SourceInfo::with_root("Shift3", "shf3", "algebra/shift.hh")
     }
 }
 
@@ -126,12 +124,38 @@ impl<A, B, T> Entity for Chain<A, B, T> where A: Map<T> + Reorder<B, T>, B: Map<
         dst.align(Self::align(cfg))?;
         Ok(())
     }
-    fn entity_source(_cfg: &Config) -> SourceInfo {
-        unimplemented!()
-        // FIXME: build source
-        //let a = A::entity_source(cfg);
-        //let b = B::entity_source(cfg);
-        //SourceInfo::new("", "", "")
+    fn entity_source(cfg: &Config) -> SourceInfo {
+        let id = TypeId::of::<Self>();
+        let aff3_id = TypeId::of::<Chain<Shift<f64, 3>, Linear<f64, 3>, Vector<f64, 3>>>();
+        let hom3_id = TypeId::of::<Chain<Shift<f64, 3>, Rotation3<f64>, Vector<f64, 3>>>();
+        if id == aff3_id {
+            unimplemented!();
+        } else if id == hom3_id {
+            SourceInfo::with_root(
+                "Homogenous3",
+                "hom3",
+                "transfomrm/homogenous.hh",
+            )
+        } else {
+            let asrc = A::entity_source(cfg);
+            let bsrc = B::entity_source(cfg);
+            let name = format!("Chain{}", Self::type_tag());
+            let prefix = format!("chain_{}", Self::type_tag());
+            SourceBuilder::new(format!("generated/chain_{}.hh", Self::type_tag()))
+                .tree(asrc.tree)
+                .tree(bsrc.tree)
+                .content(&include_template!(
+                    "transform/chain.inl",
+                    "Self": &name,
+                    "self": &prefix,
+                    "Outer": &asrc.name,
+                    "outer": &asrc.prefix,
+                    "Inner": &bsrc.name,
+                    "inner": &bsrc.prefix,
+                    "elem": T::entity_source(cfg).name,
+                ))
+                .build(name, prefix)
+        }
     }
 }
 impl<A, B, T> SizedEntity for Chain<A, B, T> where A: Map<T> + Reorder<B, T>, B: Map<T> + Reorder<A, T>, T: SizedEntity + Copy {
